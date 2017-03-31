9efd1d93f485f687ccde0951f21a06b72fcfc772
dripauwe <Dries.Pauwels@UGent.be>
2017-03-31 10:28:25 +0200

    Update server_config.sh

--- server_config.sh
+++ server_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+DIR=$PWD
 sudo apt-get update
 sudo apt-get install -y nodejs-legacy npm
 sudo npm -g install grunt-cli
@@ -13,9 +13,10 @@ sudo dpkg -i google-chrome*.deb; sudo apt-get -f install -y && sudo dpkg -i goog
 wget https://storage.googleapis.com/golang/go1.7.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7*
 export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/WebRTC-server-Virtual-Wall/go
+export GOPATH=$DIR/go
 echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
-echo "export GOPATH=$HOME/WebRTC-server-Virtual-Wall/go" >> /etc/profile
+echo "export GOPATH=$DIR/go" >> /etc/profile
+sed -n "s/ListenAndServeTLS(\"\/cert\/cert.pem\", \"\/cert\/key.pem\")/ListenAndServeTLS(\"${DIR}\/cert\/cert.pem\", \"${DIR}\/cert\/key.pem\")/g" go/src/collider/collider.go
 go install collidermain
 # libevent
 cd libevent*
@@ -30,7 +31,7 @@ grunt build
 
 cd ../
 sudo apt-get install stunnel4 -y
-echo "cert = $PWD/cert/stunnel.pem" >> stunnel.conf
+echo "cert = $DIR/cert/stunnel.pem" >> stunnel.conf
 sudo cp stunnel.conf /etc/stunnel
 sudo cp stunnel4 /etc/default/
 sudo /etc/init.d/stunnel4 restart
