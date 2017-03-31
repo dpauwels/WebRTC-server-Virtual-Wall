1113b0744cf20e303b6bcd47f38bf3b46446edf8
dripauwe <Dries.Pauwels@UGent.be>
2017-03-31 15:00:33 +0200

    Update server_config.sh

--- server_config.sh
+++ server_config.sh
@@ -66,7 +66,7 @@ sudo dpkg -i google-chrome*.deb; sudo apt-get -f install -y && sudo dpkg -i goog
 # go
 wget https://storage.googleapis.com/golang/go1.7.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7*
-echo "export PATH=$PATH" >> $HOME/.profile
+echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
 echo "export GOPATH=$DIR/go" >> $HOME/.profile
 #sed -i "s/ListenAndServeTLS(\"\/cert\/cert.pem\", \"\/cert\/key.pem\")/ListenAndServeTLS(\"${DIR}\/cert\/cert.pem\", \"${DIR}\/cert\/key.pem\")/g" go/src/collider/collider.go
 sudo cp -r ${DIR}/cert /
